20bad37df140650c2c39bd0af264ab186e865974
aleksandr <kharitonov_am@bk.ru>
2024-12-19 09:56:03 +0300

    Добавил в модуль views функцию make_interval_data

---
+++ src/views.py
@@ -0,0 +1,26 @@
+import datetime
+
+def make_interval_dates(start_data:str, diap_data:str)->str:
+    ''' Принимает дату и обозначение диапазона, возвращает стартовую дату'''
+
+    data_object = datetime.datetime.strptime(start_data, '%d.%m.%Y')
+    #обрабатываем когда подаётся W - начало интервала - понедельник текущей недели
+    if diap_data.upper() == 'W':
+        weekday_number = data_object.isoweekday()
+        data_monday = data_object - datetime.timedelta(days= (weekday_number-1))
+        return data_monday
+    # обрабатываем когда подаётся M - начало интервала - первое число этого месяца
+    elif diap_data.upper() == 'M':
+        return data_object.replace(day=1)
+    elif diap_data.upper() == 'Y':
+        return data_object.replace(day=1, month=1)
+    elif diap_data.upper() == 'ALL':
+        return 'all_data'
+    else:
+        print('Ввели некорректные данные')
+        raise ValueError
+
+
+z = make_interval_dates('18.12.2024','W')
+print(z)
+
