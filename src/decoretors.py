55cf82436fd39c435e3daa5b49b63b57a4574f53
aleksandr <kharitonov_am@bk.ru>
2024-12-23 19:12:25 +0300

    Залогировал decorators

--- src/decoretors.py
+++ src/decoretors.py
@@ -1,8 +1,19 @@
 from src.setting import Reports_log
+from logs.logs_settint import mylogconfig
+import logging
+
+#импортируем настройки логирования
+logging.basicConfig= mylogconfig
+
+#определяем именные логеры
+logging_report_log = logging.getLogger("decorator_report_log")
+
 def report_log(func):
     def wrapper(*args,**kwargs):
+        logging_report_log.info('Начало выполнение декоратора')
         rezult = func(*args, **kwargs)
         with open(Reports_log, 'a', encoding='utf-8') as f:
             f.write(f'Выполнен отчёт {func.__name__}, результат отчёта {rezult}\n')
         return rezult
+    logging_report_log.info("Декоратор отработал нормально")
     return wrapper
